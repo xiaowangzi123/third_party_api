330624c58c7c94ffe6eca43a890de8f10b42c5a4
xiaowangzi123 <1103694415@qq.com>
2022-03-17 09:09:43 +0800

    路径问题

---
+++ tts-short-domo/src/main/java/com/tts/shorts/demo/TtsShortDomoApplication.java
@@ -0,0 +1,13 @@
+package com.tts.shorts.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtsShortDomoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtsShortDomoApplication.class, args);
+    }
+
+}
---
+++ tts-short-domo/src/test/java/com/tts/shorts/demo/TtsShortDomoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.shorts.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtsShortDomoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
