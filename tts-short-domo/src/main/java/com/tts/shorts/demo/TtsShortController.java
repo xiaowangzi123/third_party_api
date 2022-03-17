9c59f59280fe4e770e86c62ac797a66a9090738b
xiaowangzi123 <1103694415@qq.com>
2022-03-17 21:07:14 +0800

    路径问题

---
+++ tts-short-domo/src/main/java/com/tts/shorts/demo/TtsShortController.java
@@ -0,0 +1,13 @@
+package com.tts.shorts.demo;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2022/3/17
+ * @description :
+ */
+public class TtsShortController {
+    public static void main(String[] args) {
+        String s1 = "https://www.aouj.joeklj.joj.aee.mp4";
+        System.out.println(s1.substring(s1.lastIndexOf(".")+1));
+    }
+}
