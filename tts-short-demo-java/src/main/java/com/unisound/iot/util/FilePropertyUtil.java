2191be2f6cbc9b3ee765ba20e967d7c2f527dd32
xiaowangzi123 <1103694415@qq.com>
2022-03-19 21:36:07 +0800

    获取视频大小时长

---
+++ tts-short-demo-java/src/main/java/com/unisound/iot/util/FilePropertyUtil.java
@@ -0,0 +1,118 @@
+package com.unisound.iot.util;
+
+
+import ws.schild.jave.MultimediaObject;
+import ws.schild.jave.info.MultimediaInfo;
+
+import java.io.*;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.channels.FileChannel;
+import java.util.UUID;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2022/3/19
+ * @description :
+ */
+public class FilePropertyUtil {
+    /**
+     * 获取音视频时长
+     *
+     * @param file 音视频文件
+     * @return 音视频时长（单位：秒）
+     * @throws EncoderException
+     */
+    public static String readVideoTime(File file) throws EncoderException {
+        Encoder encoder = new Encoder();
+
+        MultimediaInfo m = encoder.getInfo(file);
+        double time = new Long(m.getDuration()).doubleValue() / 1000;
+        return String.valueOf(time);
+    }
+
+    private static File getRemoteFile(String path) throws Exception {
+        URL url = new URL(path);
+        //链接url
+        URLConnection uc = url.openConnection();
+        InputStream in = uc.getInputStream();
+        String suffix = path.substring(path.lastIndexOf(".") + 1);
+        String prefix = "." + suffix;
+        File file = File.createTempFile(UUID.randomUUID().toString(), prefix);
+        OutputStream os = new FileOutputStream(file);
+        byte[] b = new byte[1024];
+        int readTmp;
+        while ((readTmp = in.read(b)) != -1) {
+            os.write(b, 0, readTmp);
+            os.flush();
+        }
+        return file;
+    }
+
+    /**
+     * 视频时长
+     *
+     * @param fileUrl
+     * @return
+     */
+    public static String readVideoTime(String fileUrl) {
+        File source = new File(fileUrl);
+        String length = "";
+        try {
+            MultimediaObject instance = new MultimediaObject(source);
+            MultimediaInfo result = instance.getInfo();
+            long ls = result.getDuration() / 1000;
+            int hour = (int) (ls / 3600);
+            int minute = (int) (ls % 3600) / 60;
+            int second = (int) (ls - hour * 3600 - minute * 60);
+            String hr = Integer.toString(hour);
+            String mi = Integer.toString(minute);
+            String se = Integer.toString(second);
+            if (hr.length() < 2) {
+                hr = "0" + hr;
+            }
+            if (mi.length() < 2) {
+                mi = "0" + mi;
+            }
+            if (se.length() < 2) {
+                se = "0" + se;
+            }
+            length = hr + ":" + mi + ":" + se;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return length;
+    }
+
+    /**
+     * 视频大小
+     *
+     * @param source
+     * @return
+     */
+    public static String readVideoSize(File source) {
+        FileChannel fc = null;
+        String size = "";
+        try {
+            FileInputStream fis = new FileInputStream(source);
+            fc = fis.getChannel();
+            BigDecimal fileSize = new BigDecimal(fc.size());
+            size = fileSize.divide(new BigDecimal(1024 * 1024), 2, RoundingMode.HALF_UP) + "MB";
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (null != fc) {
+                try {
+                    fc.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return size;
+    }
+}
