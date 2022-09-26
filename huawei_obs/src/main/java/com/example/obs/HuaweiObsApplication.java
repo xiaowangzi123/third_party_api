67bae2a51104444d9ba9ccd94e5d940e5046ce88
xiaowangzi123 <1103694415@qq.com>
2022-09-26 21:08:35 +0800

    华为OBS

---
+++ huawei_obs/src/main/java/com/example/obs/HuaweiObsApplication.java
@@ -0,0 +1,13 @@
+package com.example.obs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuaweiObsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuaweiObsApplication.class, args);
+    }
+
+}
