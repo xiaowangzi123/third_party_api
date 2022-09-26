a65b0e4babf7ab6e10c265543dd852e5fa820f40
xiaowangzi123 <1103694415@qq.com>
2022-09-26 21:33:17 +0800

    华为OBS

---
+++ huawei_obs/src/main/java/com/example/obs/PutObject.java
@@ -0,0 +1,14 @@
+package com.example.obs;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2022/9/26
+ * @description :
+ */
+
+@Slf4j
+public class PutObject {
+
+}
