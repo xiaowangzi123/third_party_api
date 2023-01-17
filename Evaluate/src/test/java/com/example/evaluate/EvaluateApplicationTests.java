39b080c05d98879a0864914899c81b8d9c9b355f
wyq <wangyq@waitrans.com>
2023-01-17 09:31:54 +0800

    翻译评估

---
+++ Evaluate/src/test/java/com/example/evaluate/EvaluateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.evaluate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
