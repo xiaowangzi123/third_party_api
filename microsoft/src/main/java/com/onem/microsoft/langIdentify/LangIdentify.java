2e6bc5c2a94f08332f973276297e14b0d70eb23a
xiaowangzi123 <1103694415@qq.com>
2023-01-31 14:47:08 +0800

    语种检测

--- microsoft/src/main/java/com/onem/microsoft/langIdentify/LangIdentify.java
+++ microsoft/src/main/java/com/onem/microsoft/langIdentify/LangIdentify.java
@@ -12,12 +12,13 @@ import com.azure.core.credential.AzureKeyCredential;
  */
 public class LangIdentify {
     private static String KEY = "46411c71077e47ca96f79b2108eeafe4";
-    private static String ENDPOINT = "chinaeast2";
+    private static String ENDPOINT = "https://languageservice.cognitiveservices.azure.cn/";
 
     public static void main(String[] args) {
         TextAnalyticsClient client = authenticateClient(KEY, ENDPOINT);
         detectLanguageExample(client);
     }
+
     // Method to authenticate the client object with your key and endpoint
     static TextAnalyticsClient authenticateClient(String key, String endpoint) {
         return new TextAnalyticsClientBuilder()
@@ -25,16 +26,16 @@ public class LangIdentify {
                 .endpoint(endpoint)
                 .buildClient();
     }
+
     // Example method for detecting the language of text
-    static void detectLanguageExample(TextAnalyticsClient client)
-    {
+    static void detectLanguageExample(TextAnalyticsClient client) {
         // The text to be analyzed.
-        String text = "Ce document est rédigé en Français.";
+        String text = "I am a teacher";
 
         DetectedLanguage detectedLanguage = client.detectLanguage(text);
-        System.out.printf("Detected primary language: %s, ISO 6391 name: %s, score: %.2f.%n",
-                detectedLanguage.getName(),
-                detectedLanguage.getIso6391Name(),
-                detectedLanguage.getConfidenceScore());
+        System.out.printf("-----> Detected primary language: %s, ISO 6391 name: %s, score: %.2f.%n",
+                detectedLanguage.getName(), //English
+                detectedLanguage.getIso6391Name(),  //en
+                detectedLanguage.getConfidenceScore());  //0.70
     }
 }
