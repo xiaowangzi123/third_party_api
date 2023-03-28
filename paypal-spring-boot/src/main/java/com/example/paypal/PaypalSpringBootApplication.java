0b0a9cbbcd08f2deaf06d67a34b66033e0f54c94
xiaowangzi123 <1103694415@qq.com>
2023-03-28 10:15:45 +0800

    paypal

---
+++ paypal-spring-boot/src/main/java/com/example/paypal/PaypalSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.paypal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaypalSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaypalSpringBootApplication.class, args);
+    }
+
+}
