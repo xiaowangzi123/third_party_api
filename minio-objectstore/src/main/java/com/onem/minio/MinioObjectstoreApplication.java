b117b966cf7ff21d27827c13e6fabf9047f85b55
xiaowangzi123 <1103694415@qq.com>
2023-04-26 15:55:12 +0800

    minio

---
+++ minio-objectstore/src/main/java/com/onem/minio/MinioObjectstoreApplication.java
@@ -0,0 +1,13 @@
+package com.onem.minio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinioObjectstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinioObjectstoreApplication.class, args);
+    }
+
+}
