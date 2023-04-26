05d6c9a04d02c479fdf4a4df6d2e5808badd9182
xiaowangzi123 <1103694415@qq.com>
2023-04-26 18:01:15 +0800

    解决冲突

--- minio-objectstore/src/main/java/com/onem/minio/controller/MinIoController.java
+++ minio-objectstore/src/main/java/com/onem/minio/controller/MinIoController.java
@@ -1,10 +1,12 @@
 package com.onem.minio.controller;
 
 import com.onem.minio.utils.MinioUtils;
+import io.minio.messages.Bucket;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author wyq
@@ -22,7 +24,30 @@ public class MinIoController {
 
     @PostMapping("/bucket/exist")
     public String bucketExists(@RequestParam("bucketName") String bucketName) {
-        minioUtils.bucketExist(bucketName);
+        Boolean exist = minioUtils.bucketExist(bucketName);
+        log.info("是否存在：{}",exist);
+        return "成功";
+    }
+
+    @PostMapping("/bucket/create")
+    public String bucketCreate(@RequestParam("bucketName") String bucketName) {
+        Boolean exist = minioUtils.makeBucket(bucketName);
+        log.info("是否创建成功：{}",exist);
+        return "成功";
+    }
+
+    @PostMapping("/bucket/delete")
+    public String bucketDelete(@RequestParam("bucketName") String bucketName) {
+        Boolean exist = minioUtils.removeBucket(bucketName);
+        log.info("是否删除成功：{}",exist);
+        return "成功";
+    }
+
+
+    @PostMapping("/bucket/list")
+    public String bucketList() {
+        List<Bucket> allBuckets = minioUtils.getAllBuckets();
+        log.info("是否删除成功：{}",JSON.toJSONString(allBuckets));
         return "成功";
     }
 }
