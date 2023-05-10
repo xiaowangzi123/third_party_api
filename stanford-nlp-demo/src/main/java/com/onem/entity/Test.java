987278244731fe9eac671e2efb3c6b14a979950c
xiaowangzi123 <1103694415@qq.com>
2023-05-10 14:17:25 +0800

    stanford-nlp -句子切分

---
+++ stanford-nlp-demo/src/main/java/com/onem/entity/Test.java
@@ -0,0 +1,39 @@
+package com.onem.entity;
+
+import com.alibaba.fastjson.JSON;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author wyq
+ * @date 2023/5/9
+ * @desc
+ */
+public class Test {
+    public static void main(String[] args) {
+
+        SyntaxResultDto dto = new SyntaxResultDto();
+
+        List<String> a1 = new ArrayList<String>();
+        a1.add();
+        List<List<String>> dependencyList=new ArrayList<>();
+
+
+        String s = "nn(New-11, Japan-2)\n" +
+                "conj_and(Sales-111, FoodLine-121)";
+
+
+        List<String> b1 = Arrays.asList(s.split("\\n"));
+        dependencyList.add(b1);
+
+        String s2 = "nn(New-11, Japan-2)\n" +
+                "conj_and(Sales-111, FoodLine-121)";
+
+        List<String> b2 = Arrays.asList(s2.split("\\n"));
+        dependencyList.add(b2);
+        dto.setDependencyList(dependencyList);
+        System.out.println(JSON.toJSONString(dto));
+    }
+}
--- stanford-nlp-demo/src/main/java/com/onem/utils/Test.java
+++ stanford-nlp-demo/src/main/java/com/onem/utils/Test.java
@@ -1,6 +1,7 @@
 package com.onem.utils;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -10,11 +11,36 @@ import java.util.List;
  */
 public class Test {
     public static void main(String[] args) {
+        List<String> objects = Collections.emptyList();
+        System.out.println(objects.size());
         List<String> arr = new ArrayList<String>();
         for (int i = 51; i <= 100; i++) {
             arr.add("\"" + i + "\"");
         }
         System.out.println(arr);
 
+        List<List<String>> lists = splitList(arr, 8);
+        System.out.println(lists.size());
+    }
+
+    private static <T> List<List<T>> splitList(List<T> list, int subNum) {
+        List<List<T>> tNewList = new ArrayList<List<T>>();
+        int priIndex = 0;
+        int lastPriIndex = 0;
+        int insertTimes = list.size() / subNum;
+        List<T> subList = new ArrayList<>();
+        for (int i = 0; i <= insertTimes; i++) {
+            priIndex = subNum * i;
+            lastPriIndex = priIndex + subNum;
+            if (i == insertTimes) {
+                subList = list.subList(priIndex, list.size());
+            } else {
+                subList = list.subList(priIndex, lastPriIndex);
+            }
+            if (subList.size() > 0) {
+                tNewList.add(subList);
+            }
+        }
+        return tNewList;
     }
 }
