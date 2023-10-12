66db8bc5fbfbe4a06dce4732299921b13ff057d2
xiaowangzi123 <1103694415@qq.com>
2023-10-12 16:12:21 +0800

    云知声

---
+++ unisound/src/main/java/com/unisound/UnisoundApplication.java
@@ -0,0 +1,13 @@
+package com.unisound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnisoundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnisoundApplication.class, args);
+    }
+
+}
---
+++ unisound/src/test/java/com/unisound/UnisoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unisound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnisoundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
