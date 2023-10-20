fae57a565d89612c7a5583ece5f54d59ad904700
xiaowangzi123 <1103694415@qq.com>
2023-10-20 17:51:40 +0800

    PayPalClient

--- paypal-spring-boot/src/main/java/com/example/paypal/CaptureIntentExamples/CreateOrder.java
+++ paypal-spring-boot/src/main/java/com/example/paypal/CaptureIntentExamples/CreateOrder.java
@@ -1,10 +1,22 @@
-/*
 package com.example.paypal.CaptureIntentExamples;
 
-import com.paypal.PayPalClient;
+import com.example.paypal.enums.ItemCategoryEnum;
+import com.example.paypal.example.PayPalClient;
 import com.paypal.http.HttpResponse;
 import com.paypal.http.serializer.Json;
-import com.paypal.orders.*;
+import com.paypal.orders.AddressPortable;
+import com.paypal.orders.AmountBreakdown;
+import com.paypal.orders.AmountWithBreakdown;
+import com.paypal.orders.ApplicationContext;
+import com.paypal.orders.Item;
+import com.paypal.orders.LinkDescription;
+import com.paypal.orders.Money;
+import com.paypal.orders.Name;
+import com.paypal.orders.Order;
+import com.paypal.orders.OrderRequest;
+import com.paypal.orders.OrdersCreateRequest;
+import com.paypal.orders.PurchaseUnitRequest;
+import com.paypal.orders.ShippingDetail;
 import org.json.JSONObject;
 
 import java.io.IOException;
@@ -13,98 +25,80 @@ import java.util.List;
 
 public class CreateOrder extends PayPalClient {
 
-	*/
-/**
-	 * Method to generate sample create order body with <b>CAPTURE</b> intent
-	 *
-	 * @return OrderRequest with created order request
-	 *//*
 
-	private OrderRequest buildRequestBody() {
-		OrderRequest orderRequest = new OrderRequest();
-		orderRequest.checkoutPaymentIntent("CAPTURE");
+    private OrderRequest buildRequestBody() {
+        OrderRequest orderRequest = new OrderRequest();
+        orderRequest.checkoutPaymentIntent("CAPTURE");
 
-		ApplicationContext applicationContext = new ApplicationContext().brandName("EXAMPLE INC").landingPage("BILLING")
-				.cancelUrl("https://www.example.com").returnUrl("https://www.example.com").userAction("CONTINUE")
-				.shippingPreference("SET_PROVIDED_ADDRESS");
-		orderRequest.applicationContext(applicationContext);
+        ApplicationContext applicationContext = new ApplicationContext().brandName("EXAMPLE INC").landingPage("BILLING")
+                .cancelUrl("https://www.example.com")
+                .returnUrl("https://www.example.com")
+                .userAction("CONTINUE")
+                .shippingPreference("SET_PROVIDED_ADDRESS");
+        orderRequest.applicationContext(applicationContext);
 
-		List<PurchaseUnitRequest> purchaseUnitRequests = new ArrayList<PurchaseUnitRequest>();
-		PurchaseUnitRequest purchaseUnitRequest = new PurchaseUnitRequest().referenceId("PUHF")
-				.description("Sporting Goods").customId("CUST-HighFashions").softDescriptor("HighFashions")
-				.amountWithBreakdown(new AmountWithBreakdown().currencyCode("USD").value("220.00")
-						.amountBreakdown(new AmountBreakdown().itemTotal(new Money().currencyCode("USD").value("180.00"))
-								.shipping(new Money().currencyCode("USD").value("20.00"))
-								.handling(new Money().currencyCode("USD").value("10.00"))
-								.taxTotal(new Money().currencyCode("USD").value("20.00"))
-								.shippingDiscount(new Money().currencyCode("USD").value("10.00"))))
-				.items(new ArrayList<Item>() {
-					{
-						add(new Item().name("T-shirt").description("Green XL").sku("sku01")
-								.unitAmount(new Money().currencyCode("USD").value("90.00"))
-								.tax(new Money().currencyCode("USD").value("10.00")).quantity("1")
-								.category("PHYSICAL_GOODS"));
-						add(new Item().name("Shoes").description("Running, Size 10.5").sku("sku02")
-								.unitAmount(new Money().currencyCode("USD").value("45.00"))
-								.tax(new Money().currencyCode("USD").value("5.00")).quantity("2")
-								.category("PHYSICAL_GOODS"));
-					}
-				})
-				.shippingDetail(new ShippingDetail().name(new Name().fullName("John Doe"))
-						.addressPortable(new AddressPortable().addressLine1("123 Townsend St").addressLine2("Floor 6")
-								.adminArea2("San Francisco").adminArea1("CA").postalCode("94107").countryCode("US")));
-		purchaseUnitRequests.add(purchaseUnitRequest);
-		orderRequest.purchaseUnits(purchaseUnitRequests);
-		return orderRequest;
-	}
+        List<PurchaseUnitRequest> purchaseUnitRequests = new ArrayList<PurchaseUnitRequest>();
+        PurchaseUnitRequest purchaseUnitRequest = new PurchaseUnitRequest().referenceId("PUHF")
+                .description("Sporting Goods").customId("CUST-HighFashions").softDescriptor("HighFashions")
+                .amountWithBreakdown(new AmountWithBreakdown().currencyCode("USD").value("22.00")
+                        .amountBreakdown(new AmountBreakdown().itemTotal(new Money().currencyCode("USD").value("18.00"))
+                                .shipping(new Money().currencyCode("USD").value("2.00"))
+                                .handling(new Money().currencyCode("USD").value("1.00"))
+                                .taxTotal(new Money().currencyCode("USD").value("2.00"))
+                                .shippingDiscount(new Money().currencyCode("USD").value("1.00"))))
+                .items(new ArrayList<Item>() {
+                    {
+                        add(new Item().name("转写视频01.mp4").description("视频转写").sku("sku01")
+                                .unitAmount(new Money().currencyCode("USD").value("9.00"))
+                                .tax(new Money().currencyCode("USD").value("1.00")).quantity("1")
+                                .category(ItemCategoryEnum.DIGITAL_GOODS.name()));
+                        add(new Item().name("转写视频01.mp4").description("视频转写").sku("sku02")
+                                .unitAmount(new Money().currencyCode("USD").value("4.0"))
+                                .tax(new Money().currencyCode("USD").value("5.0")).quantity("1")
+                                .category(ItemCategoryEnum.DIGITAL_GOODS.name()));
+                    }
+                })
+                .shippingDetail(new ShippingDetail().name(new Name().fullName("John Doe"))
+                        .addressPortable(new AddressPortable().addressLine1("123 Townsend St").addressLine2("Floor 6")
+                                .adminArea2("San Francisco").adminArea1("CA").postalCode("94107").countryCode("US")));
+        purchaseUnitRequests.add(purchaseUnitRequest);
+        orderRequest.purchaseUnits(purchaseUnitRequests);
+        return orderRequest;
+    }
 
-	*/
-/**
-	 * Method to create order
-	 *
-	 * @param debug true = print response data
-	 * @return HttpResponse<Order> response received from API
-	 * @throws IOException Exceptions from API if any
-	 *//*
 
-	public HttpResponse<Order> createOrder(boolean debug) throws IOException {
-		OrdersCreateRequest request = new OrdersCreateRequest();
-		request.header("prefer","return=representation");
-		request.requestBody(buildRequestBody());
-		HttpResponse<Order> response = client().execute(request);
-		if (debug) {
-			if (response.statusCode() == 201) {
-				System.out.println("Status Code: " + response.statusCode());
-				System.out.println("Status: " + response.result().status());
-				System.out.println("Order ID: " + response.result().id());
-				System.out.println("Intent: " + response.result().checkoutPaymentIntent());
-				System.out.println("Links: ");
-				for (LinkDescription link : response.result().links()) {
-					System.out.println("\t" + link.rel() + ": " + link.href() + "\tCall Type: " + link.method());
-				}
-				System.out.println("Total Amount: " + response.result().purchaseUnits().get(0).amountWithBreakdown().currencyCode()
-						+ " " + response.result().purchaseUnits().get(0).amountWithBreakdown().value());
-				System.out.println("Full response body:");
-				System.out.println(new JSONObject(new Json().serialize(response.result())).toString(4));
-			}
-		}
-		return response;
-	}
+    public HttpResponse<Order> createOrder(boolean debug) throws IOException {
+        OrdersCreateRequest request = new OrdersCreateRequest();
+        request.header("prefer", "return=representation");
+        request.requestBody(buildRequestBody());
+        HttpResponse<Order> response = client().execute(request);
+        System.out.println("创建订单返回--->" + response);
+        if (response.statusCode() == 201) {
+            System.out.println("Status Code: " + response.statusCode());
+            System.out.println("Status: " + response.result().status());
+            System.out.println("Order ID: " + response.result().id());
+            System.out.println("Intent: " + response.result().checkoutPaymentIntent());
+            System.out.println("Links: ");
+            for (LinkDescription link : response.result().links()) {
+                System.out.println("\t" + link.rel() + ": " + link.href() + "\tCall Type: " + link.method());
+            }
+            System.out.println("Total Amount: " + response.result().purchaseUnits().get(0).amountWithBreakdown().currencyCode()
+                    + " " + response.result().purchaseUnits().get(0).amountWithBreakdown().value());
+            System.out.println("Full response body:");
+            System.out.println(new JSONObject(new Json().serialize(response.result())).toString(4));
+        }
 
-	*/
-/**
-	 * This is the driver function which invokes the createOrder function to create
-	 * an sample order.
-	 *//*
+        return response;
+    }
 
-	public static void main(String args[]) {
-		try {
-			new CreateOrder().createOrder(true);
-		} catch (com.paypal.http.exceptions.HttpException e) {
-			System.out.println(e.getLocalizedMessage());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+
+    public static void main(String args[]) {
+        try {
+            new CreateOrder().createOrder(true);
+        } catch (com.paypal.http.exceptions.HttpException e) {
+            System.out.println(e.getLocalizedMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
-*/
--- paypal-spring-boot/src/main/java/com/example/paypal/example/GetOrder.java
+++ paypal-spring-boot/src/main/java/com/example/paypal/example/GetOrder.java
@@ -1,7 +1,6 @@
 package com.example.paypal.example;
 
 import com.example.paypal.AuthorizeIntentExamples.CreateOrder;
-import com.example.paypal.client.PayPalClient;
 import com.paypal.http.HttpResponse;
 import com.paypal.http.serializer.Json;
 import com.paypal.orders.Order;
@@ -13,7 +12,6 @@ import java.io.IOException;
 public class GetOrder extends PayPalClient {
 
 
-	
 	public void getOrder(String orderId) throws IOException {
 		OrdersGetRequest request = new OrdersGetRequest(orderId);
 		HttpResponse<Order> response = client().execute(request);
