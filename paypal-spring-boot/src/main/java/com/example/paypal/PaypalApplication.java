8dbf9015060808711398ac7122658acf1162adde
xiaowangzi123 <1103694415@qq.com>
2023-10-23 15:09:17 +0800

    PayPalClient

--- paypal-spring-boot/src/main/java/com/example/paypal/PaypalApplication.java
+++ paypal-spring-boot/src/main/java/com/example/paypal/PaypalApplication.java
@@ -3,9 +3,8 @@ package com.example.paypal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example.paypal.*"})
 public class PaypalApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PaypalApplication.class, args);
     }
--- paypal-spring-boot/src/main/java/com/example/paypal/controller/PayPalCheckoutController.java
+++ paypal-spring-boot/src/main/java/com/example/paypal/controller/PayPalCheckoutController.java
@@ -2,20 +2,20 @@ package com.example.paypal.controller;
 
 import com.example.paypal.service.PayPalCheckoutService;
 import com.example.paypal.utils.RequestToMapUtil;
-import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
-@Api(description = "PayPalCheckout接口")
+@RequestMapping(value = "/paypal")
 public class PayPalCheckoutController {
 
-    @Autowired
+    @Resource
     private PayPalCheckoutService payPalCheckoutService;
 
     @ApiOperation(value = "ipn异步回调")
