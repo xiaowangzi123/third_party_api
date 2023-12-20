958461d60f932eb94ac99bf59ebd4fcc90923b53
xiaowangzi123 <1103694415@qq.com>
2023-12-20 09:43:14 +0800

    obs

--- huawei_obs_java/src/test/java/com/example/obs/ListObjectsTest.java
+++ huawei_obs_java/src/test/java/com/example/obs/ListObjectsTest.java
@@ -36,7 +36,8 @@ public class ListObjectsTest {
         List<String> objectList = new ArrayList<>();
         try (final ObsClient obsClient = new ObsClient(ak, sk, endPoint);) {
 //            ListObjectsRequest request = new ListObjectsRequest("cloudtranslation");
-            ListObjectsRequest request = new ListObjectsRequest("tqe-cloudtranslation-dev");
+            ListObjectsRequest request = new ListObjectsRequest("transwai-prod");
+//            ListObjectsRequest request = new ListObjectsRequest("tqe-cloudtranslation-dev");
 //            ListObjectsRequest request = new ListObjectsRequest("tqe-cloudtranslation-test");
 //            ListObjectsRequest request = new ListObjectsRequest("tqe-cloudtranslation-prod");
             request.setMaxKeys(1000);
