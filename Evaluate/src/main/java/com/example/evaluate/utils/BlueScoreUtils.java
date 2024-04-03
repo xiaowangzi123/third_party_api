2f46af276f7e94bba35195fa51ac19f37867897b
xiaowangzi123 <1103694415@qq.com>
2024-04-03 11:28:36 +0800

    Blue

--- Evaluate/src/main/java/com/example/evaluate/utils/BlueScoreUtils.java
+++ Evaluate/src/main/java/com/example/evaluate/utils/BlueScoreUtils.java
@@ -1,9 +1,35 @@
 package com.example.evaluate.utils;
 
+import com.newtranx.eval.metrics.IEvaluate;
+import com.newtranx.eval.metrics.MetricUtil;
+import com.newtranx.eval.metrics.Score;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
 /**
  * @author wyq
  * @date 2024/4/3
  * @desc
  */
 public class BlueScoreUtils {
+
+    public static void main(String[] args) {
+
+//        String lang = "zh";
+        String lang = "en";
+        String trans = "Going to play basketball in the afternoon ?";
+        List<String> referenceList = new ArrayList<>(Arrays.asList("Going to play basketball this afternoon ?"
+                , "Going to play basketball afternoon ?"));
+
+        getScore(lang, trans, referenceList);
+    }
+
+    public static void getScore(String langCode, String trans, List<String> referenceList) {
+        IEvaluate evaluate = MetricUtil.buildBleuMetric(langCode);
+        Score score = evaluate.sentenceScore(trans, referenceList);
+        System.out.println(score);
+    }
 }
